6f04a2844b41087d7cd4d051b194dc8f672ce41b
jidosx <jsd19038@gmail.com>
2025-02-14 21:10:06 -0500

    Create test_models.py

---
+++ tests/init.py/test_models.py
@@ -0,0 +1,14 @@
+import unittest
+from neuratrade.models.neural_network import NeuralNetwork
+
+class TestNeuralNetwork(unittest.TestCase):
+    def test_train(self):
+        # TO DO: Implement test for train method
+        pass
+
+    def test_predict(self):
+        # TO DO: Implement test for predict method
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
