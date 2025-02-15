25b0d0eed7ae463b682154ef76c9519b73495096
jidosx <jsd19038@gmail.com>
2025-02-14 21:07:12 -0500

    Create example_strategy.py

---
+++ init.py/strategies/example_strategy.py
@@ -0,0 +1,6 @@
+from .base_strategy import BaseStrategy
+
+class ExampleStrategy(BaseStrategy):
+    def generate_signal(self, data):
+        # TO DO: Implement signal generation logic
+        pass
