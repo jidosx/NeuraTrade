9a087795fa5e597bfc3affc78b4ebf7fa2676046
jidosx <jsd19038@gmail.com>
2025-02-14 21:06:13 -0500

    Create base_model.py

---
+++ init.py/model/base_model.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+class BaseModel(ABC):
+    @abstractmethod
+    def train(self, data):
+        pass
+
+    @abstractmethod
+    def predict(self, data):
+        pass
