f33f2af7112220432dd34bc98d4ada76faf56e21
jidosx <jsd19038@gmail.com>
2025-02-14 21:09:36 -0500

    Create test_data_loaders.py

---
+++ tests/init.py/test_data_loaders.py
@@ -0,0 +1,10 @@
+import unittest
+from neuratrade.data.loaders import load_data
+
+class TestDataLoaders(unittest.TestCase):
+    def test_load_data(self):
+        # TO DO: Implement test for load_data function
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
