8bf8188e074eb1d19c39eab52a6dbf3eb8a113ec
jidosx <jsd19038@gmail.com>
2025-02-14 21:10:19 -0500

    Create test_strategies.py

---
+++ tests/init.py/test_strategies.py
@@ -0,0 +1,10 @@
+import unittest
+from neuratrade.strategies.example_strategy import ExampleStrategy
+
+class TestExampleStrategy(unittest.TestCase):
+    def test_generate_signal(self):
+        # TO DO: Implement test for generate_signal method
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
