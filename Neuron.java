1719185286360c125f1a60f18faa7c812e89c9a1
jidosx <jsd19038@gmail.com>
2025-02-18 02:49:38 -0500

    Create Neuron.java

---
+++ Neuron.java
@@ -0,0 +1,15 @@
+public class Neuron {
+    private double output;
+
+    public Neuron() {
+        this.output = 0.0;
+    }
+
+    public void setOutput(double output) {
+        this.output = output;
+    }
+
+    public double getOutput() {
+        return this.output;
+    }
+}
