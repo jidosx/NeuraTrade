9bf6c8a6f66866328800c102846353566508b9c1
jidosx <jsd19038@gmail.com>
2025-02-18 02:57:52 -0500

    Create BitfinexPlugin.java

---
+++ Plugin.java/BitfinexPlugin.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class BitfinexPlugin {
+    private String apiKey;
+    private String apiSecret;
+
+    public BitfinexPlugin(String apiKey, String apiSecret) {
+        this.apiKey = apiKey;
+        this.apiSecret = apiSecret;
+    }
+
+    public String getLivePrice(String symbol) throws Exception {
+        URL url = new URL("https://api.bitfinex.com/v1/pubticker/" + symbol);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("bfx-api-key", this.apiKey);
+        connection.setRequestProperty("bfx-api-sign", this.apiSecret);
+
+        int responseCode = connection.getResponseCode();
+        if (responseCode == 200) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            return response.toString();
+        } else {
+            return null;
+        }
+    }
+}
